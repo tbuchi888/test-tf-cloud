bfd5df568454274ab35617d87f117d01c729be28
Takuya Kawabuchi <tbuchi888@users.noreply.github.com>
2022-03-03 12:25:33 +0900

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "demo-terraform"
     storage_account_name = "tbuchi888tfstate"
     container_name       = "backendcontainer"
-    key                  = "packer-custom-vm-terraform.tfstate"
+    key                  = "test-tf-cloud001.tfstate"
   }
 }
